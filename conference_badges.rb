05e40129fccd01a2596ebc397b23e99da4bd8ffc
Claudeegarcia7 <Claudeegarcia@icloud.com>
2020-10-31 03:30:32 -0500

    commit tree 6344e48ce338e0cacaf07d92cacc2780c581dcb6

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+#badge_maker
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+#batch_badge_creator
+
+def batch_badge_creator (attendees)
+    badges = []
+    attendees.each do |name|
+        badges.push badge_maker(name)
+    end
+    badges
+end
+
+def assign_rooms(names)
+    room_assigments = []
+    names.each_with_index do | name, index |
+        room_assigments.push "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    end
+    room_assigments
+end
+
+def printer(names)
+    batch_badge_creator(names).each do |badge|
+        puts badge
+    end
+        assign_rooms(names).each do |room|
+            puts room
+        end
+    end
